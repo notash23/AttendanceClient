75bb0a21726b64c644ab0caff273add1117cc764
notash23 <87333494+notash23@users.noreply.github.com>
2023-05-07 13:15:12 +0400

    Added test file for GPIOs

--- gpio.py
+++ gpio.py
@@ -1,4 +1,7 @@
+from time import sleep
+
 import OPi.GPIO as GPIO
+
 
 GPIO.setmode(GPIO.BOARD)
 button1 = 8
@@ -8,3 +11,4 @@ GPIO.setup(button1, GPIO.IN)
 while True:
     if GPIO.input(button1):
         print("Button 1 has been pressed")
+    sleep(1)
