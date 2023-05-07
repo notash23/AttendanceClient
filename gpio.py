133967377b9a514d10e81d839374bde69bc8f9e3
notash23 <87333494+notash23@users.noreply.github.com>
2023-05-07 13:05:04 +0400

    Added test file for GPIOs

--- gpio.py
+++ gpio.py
@@ -1,8 +1,7 @@
 import OPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-button1 = 72
-button2 = 75
+button1 = 15
 
 GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -10,5 +9,3 @@ GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True:
     if GPIO.input(button1):
         print("Button 1 has been pressed")
-    elif GPIO.input(button2):
-        print("Button 2 has been pressed")
