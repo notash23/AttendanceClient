01edfdbd13ccd8b7aa053b0a9cd3491cd840dc38
notash23 <87333494+notash23@users.noreply.github.com>
2023-05-10 21:55:42 +0400

    added buttons for staff select

--- gpio.py
+++ gpio.py
@@ -1,9 +1,13 @@
 import OPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.SUNXI)
-GPIO.setup("PC8", GPIO.IN, GPIO.HIGH)
+buttons = ["PC8", "PC11"]
+GPIO.setup(buttons, GPIO.IN, GPIO.HIGH)
 print("waiting")
-GPIO.wait_for_edge("PC8", GPIO.FALLING)
+GPIO.wait_for_edge(buttons[0], GPIO.FALLING)
 print("falling")
+GPIO.wait_for_edge(buttons[0], GPIO.FALLING)
+print("falling again")
 GPIO.cleanup()
 
+886
