8d035e9dbc314886c39a186e602af7d4489e8951
notash23 <87333494+notash23@users.noreply.github.com>
2023-05-10 22:40:45 +0400

    added buttons for staff select

--- gpio.py
+++ gpio.py
@@ -1,12 +1,12 @@
 import OPi.GPIO as GPIO
 
 
-def thread1():
+def thread1(button):
     print("button1")
 
 
 GPIO.setmode(GPIO.SUNXI)
-buttons = "PC8"
+buttons = ["PC8"]
 GPIO.setup(buttons, GPIO.IN, GPIO.HIGH)
 print("waiting")
 GPIO.add_event_detect(buttons[0], trigger=GPIO.FALLING, callback=thread1)
